777dc4fb842e4c923e05a706a7a0935e0ef2cb3f
xian <shanema51@gmail.com>
2024-09-30 18:48:45 -0600

    task 3 - print version

--- app/main.c
+++ app/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "../src/lab.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-  printf("hello world\n");
+  parse_args(argc, argv);
   return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,34 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+#include <stdio.h>
+
+char *get_prompt(const char *env);
+
+int change_dir(char **dir);
+
+char **cmd_parse(char const *line);
+
+void cmd_free(char **line);
+
+char *trim_white(char *line);
+
+bool do_builtin(struct shell *sh, char **argv);
+
+void sh_init(struct shell *sh);
+
+void sh_destroy(struct shell *sh);
+
+// Parse command line arguments
+void parse_args(int argc, char **argv) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                printf("v%d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+                exit(0);
+            default:
+                fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+                exit(1);
+        }
+    }
+}
