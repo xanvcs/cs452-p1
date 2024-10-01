e23da521191dd8c2350155ab806839a01be62567
xian <shanema51@gmail.com>
2024-09-30 20:03:17 -0600

    task 6 - built in commands

--- src/lab.c
+++ src/lab.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <readline/history.h>
+#include <signal.h>
+#include <pwd.h>
 
 char *get_prompt(const char *env) {
     const char *prompt_env = getenv(env);
@@ -28,7 +33,30 @@ char *get_prompt(const char *env) {
     return prompt;
 }
 
-int change_dir(char **dir);
+int change_dir(char **dir) {
+    const char *new_dir;
+
+    if (dir[0] == NULL) {
+        new_dir = getenv("HOME");
+        if (new_dir == NULL) {
+            struct passwd *pw = getpwuid(getuid());
+            if (pw == NULL) {
+                fprintf(stderr, "Error: Unable to get home directory\n");
+                return -1;
+            }
+            new_dir = pw->pw_dir;
+        }
+    } else {
+        new_dir = dir[0];
+    }
+
+    if (chdir(new_dir) != 0) {
+        perror("cd");
+        return -1;
+    }
+
+    return 0;
+}
 
 char **cmd_parse(char const *line);
 
@@ -36,11 +64,60 @@ void cmd_free(char **line);
 
 char *trim_white(char *line);
 
-bool do_builtin(struct shell *sh, char **argv);
+bool do_builtin(struct shell *sh, char **argv) {
+    if (argv[0] == NULL) {
+        return false;
+    }
 
-void sh_init(struct shell *sh);
+    if (strcmp(argv[0], "exit") == 0) {
+        printf("Exiting shell...\n");
+        sh_destroy(sh);
+        exit(0);
+    }
 
-void sh_destroy(struct shell *sh);
+    if (strcmp(argv[0], "cd") == 0) {
+        return change_dir(argv + 1);
+    }
+
+    if (strcmp(argv[0], "history") == 0) {
+        HIST_ENTRY *entry;
+        int i;
+
+        for (i = 0; i < history_length; i++) {
+            entry = history_get(i + 1);
+            if (entry) {
+                printf("%d: %s\n", i + 1, entry->line);
+            }
+        }
+        return true;
+    }
+
+    return false;
+}
+
+void sh_init(struct shell *sh) {
+    sh->shell_terminal = STDIN_FILENO;
+    sh->shell_is_interactive = isatty(sh->shell_terminal);
+    sh->prompt = NULL;
+
+    if (sh->shell_is_interactive) {
+        while (tcgetpgrp(sh->shell_terminal) != (sh->shell_pgid = getpgrp())) {
+            kill(-sh->shell_pgid, SIGTTIN);
+        }
+        tcgetattr(sh->shell_terminal, &sh->shell_tmodes);
+    }
+
+    using_history();
+}
+
+void sh_destroy(struct shell *sh) {
+    if (sh != NULL) {
+        if (sh->prompt != NULL) {
+            free(sh->prompt);
+        }
+        free(sh);
+    }
+}
 
 void parse_args(int argc, char **argv) {
     int opt;
